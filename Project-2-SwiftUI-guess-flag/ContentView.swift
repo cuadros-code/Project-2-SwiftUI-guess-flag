e7c26807d7e006a3e0d345b0f94c7b724d3470b8
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-23 15:32:31 -0500

    Initial Commit

---
+++ Project-2-SwiftUI-guess-flag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-2-SwiftUI-guess-flag
+//
+//  Created by Kevin Cuadros on 23/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-2-SwiftUI-guess-flag/Project_2_SwiftUI_guess_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2_SwiftUI_guess_flagApp.swift
+//  Project-2-SwiftUI-guess-flag
+//
+//  Created by Kevin Cuadros on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_2_SwiftUI_guess_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
