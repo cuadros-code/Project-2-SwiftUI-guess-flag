225e22424c8df8667d62ffbf064863b846c6e5f2
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-24 17:28:28 -0500

    Add assets and examplex code

--- Project-2-SwiftUI-guess-flag/ContentView.swift
+++ Project-2-SwiftUI-guess-flag/ContentView.swift
@@ -8,14 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alert = false
+    
     var body: some View {
-        VStack {
-          
+        Button("Delete Selection", role: .destructive) {
+            print("Delete selection")
         }
-        .padding()
+        
+        Button("New game", action: newGame)
+        
+        VStack {
+            Button("Button 1"){}
+                .buttonStyle(.bordered)
+            
+            Button("Button 2", role: .destructive){}
+                .buttonStyle(.bordered)
+            
+            Button("Button 3"){}
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+            
+            Button("Button 4", role: .destructive){}
+                .buttonStyle(.bordered)
+            
+            Button {
+                print("Button 5")
+            } label: {
+                Text("Button 5")
+                    .padding()
+                    .background(.blue)
+                    .foregroundColor(.white)
+            }
+            
+            .alert("Title Alert", isPresented: $alert) {
+                Button("Ok"){
+                }
+            } message: {
+                Text("This is a message")
+                Image(systemName: "square.and.arrow.up")
+            }
+        }
+    }
+    
+    func newGame() {
+        print("New Game")
+        alert.toggle()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+//LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+//    .ignoresSafeArea()
+
+//ZStack{
+//    VStack(spacing: 0){
+//        Color.red
+//        Color.blue
+//    }
+//    
+//    Text("Hello, World!")
+//        .padding(40)
+//        .background(.ultraThinMaterial)
+//}
+//.ignoresSafeArea()
+
+
+//Text("Your content")
+//    .frame(maxWidth: .infinity, maxHeight: .infinity)
+//    .foregroundStyle(.white)
+//    .background(.blue.gradient)
+
+
+//LinearGradient(stops: [
+//    Gradient.Stop(color: .white, location: 0.45),
+//    Gradient.Stop(color: .black, location: 0.55),
+//], startPoint: .top, endPoint: .bottom)
+
+
+//RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+
+
+//AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
